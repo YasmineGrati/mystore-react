138400d78bd473b8f0b1f567f064a9e192fd8936
YasmineGrati <yasmine.grati@esprit.tn>
2022-02-24 22:15:13 +0100

    Add button 'new product'

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -3,18 +3,88 @@ import styled from "styled-components";
 //import products from "../products.json";
 import Product from "./Product";
 import {useApi} from "../hooks/useApi";
-export default function Products() {
+import {queryApi} from "../utils/queryApi";
+
+export default function Products(props) {
     const[result,error,reload] = useApi("products");
+    //const [products, err, reload] = useApi('products');
     return (
+        <>
+        <Footer>
+                <Button onClick={
+                    () => props.history.replace("/addproduct")
+                }>Add new product</Button>
+        </Footer>
         <ProductsWrapper> {
             result?.map((product, index) => (
                 <Product product={product}
-                    key={index}></Product>
+                    key={index}>
+                </Product>
             ))
         } </ProductsWrapper>
+        </>
     );
 }
 const ProductsWrapper = styled.div `
  text-align: center; 
  display: flex; 
+`;
+const Errors = styled.p `
+  color: red;
+`;
+const Footer = styled.footer `
+  background: transparent;
+  grid-area: footer;
+  padding: 0.15rem;
+  text-align: right !important;
+`;
+const Search = styled.footer `
+  background: transparent;
+  grid-area: footer;
+  padding: 0.15rem;
+  text-align: center !important;
+`;
+const Button = styled.button `
+  /* Adapt the colors based on primary prop */
+  background: ${
+    (props) => (props.primary ? "palevioletred" : "white")
+};
+  color: ${
+    (props) => (props.primary ? "white" : "palevioletred")
+};
+
+  font-size: 1.5em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+const FormGroup = styled.div `
+  margin: 10px 0;
+  display: flex;
+  flex-direction: column;
+`;
+const FormField = styled.input `
+  color: black;
+  padding: 15px;
+  outline: 0;
+  border-width: 0 0 2px;
+  border-color: #ebebeb;
+  ::placeholder {
+    text-transform: uppercase;
+    font-family: "Kiona";
+    font-size: large;
+    letter-spacing: 0.1rem;
+  }
+`;
+const FormButton = styled.button `
+  background: #7b1bf7;
+  text-transform: uppercase;
+  color: white;
+  border-radius: 25px;
+  padding: 15px;
+  border: 0;
+  font-size: large;
+  margin: 10px 0;
+  font: 200 larger Kiona;
 `;
