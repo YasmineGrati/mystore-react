c7446de078dc07247ca41e9ef859b8e2ff36f38f
YasmineGrati <yasmine.grati@esprit.tn>
2022-02-24 14:33:20 +0100

    Update Product.js
    Appel de la liste des produits à partir BD

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-import products from "../products.json";
+//import products from "../products.json";
 import Product from "./Product";
+import {useApi} from "../hooks/useApi";
 export default function Products() {
+    const[result,error,reload] = useApi("products");
     return (
         <ProductsWrapper> {
-            products.map((product, index) => (
+            result?.map((product, index) => (
                 <Product product={product}
                     key={index}></Product>
             ))
